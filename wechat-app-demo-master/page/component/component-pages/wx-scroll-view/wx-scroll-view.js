f4f17de0738f08c5f22445dafd8c273d5f1e6367
272584481@qq.com <wang7255185>
2017-05-31 15:13:11 +0800

    微信小程序UI
    初次上传

---
+++ wechat-app-demo-master/page/component/component-pages/wx-scroll-view/wx-scroll-view.js
@@ -0,0 +1,15 @@
+var order = ['green', 'red', 'yellow', 'blue', 'green']
+Page({
+  data: {
+    toView: "green"
+  },
+  upper: function(e) {
+    console.log(e)
+  },
+  lower: function(e) {
+    console.log(e)
+  },
+  scroll: function(e) {
+    console.log(e)
+  },
+})
